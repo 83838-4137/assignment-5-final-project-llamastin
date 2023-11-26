7d42582ee972736c87dd2b16e0cce0c9734e095b
llamastin <j.luong@unf.edu>
2023-11-26 12:46:02 -0500

    carousel solved

--- js/script.js
+++ js/script.js
@@ -8,51 +8,52 @@ hamburgerMenu.addEventListener('click', () => {
 
 var previousButton, nextButton;
 var carouselContainer, cards, pageDots;
-var leftSlideIndex = 0;
+var leftSlideIndex = 0; 
 
 window.addEventListener('DOMContentLoaded', function (e) {
-    previousButton = document.querySelector('.previous');
-    nextButton = document.querySelector('.next');
-    carouselContainer = document.querySelector('.cards');
-    cards = document.querySelector('slide');
-    pageDots = document.querySelector('.dots li');
+  previousButton = document.querySelector('.previous');
+  nextButton = document.querySelector('.next');
+  carouselContainer = document.querySelector('.cards');
+  cards = carouselContainer.querySelectorAll('.slide');
+  pageDots = document.querySelectorAll('.dots li');
 
-    previousButton.addEventListener('click', previousSlide);
-    nextButton.addEventListener('click', nextSlide);
-
-    pageDots.forEach(function (dot) { 
-      dot.addEventListener('click', function (e) { 
-    goToSlide(Array.prototype.slice.call(pageDots).indexOf(e.target));
-    })
+  previousButton.addEventListener('click', previousSlide);
+  nextButton.addEventListener('click', nextSlide);
+  pageDots.forEach(function (dot) {
+    dot.addEventListener('click', function (e) {
+      goToSlide(Array.prototype.slice.call(pageDots).indexOf(e.target)); 
+    });
   });
 });
 
 function previousSlide() {
-  if (leftSlideIndex > 0) {
+  if (leftSlideIndex > 0) { 
     goToSlide(leftSlideIndex - 1);
   } else {
     goToSlide(cards.length - 1);
   }
 }
+
 function nextSlide() {
-  if (leftSlideIndex < cards.length - 1) {
+  if (leftSlideIndex < cards.length - 1) { 
     goToSlide(leftSlideIndex + 1);
   } else {
     goToSlide(0);
   }
 }
+
 function goToSlide(nextLeftSlideIndex) {
   $(carouselContainer).animate({
     scrollLeft: carouselContainer.offsetWidth / 3 * nextLeftSlideIndex },
-    {
-      duration: 200 });
-    
-pageDots.forEach(function (dot) {
-  dot.classList.remove('active');
-});
+  {
+    duration: 200 });
 
-pageDots[nextLeftSlideIndex].classList.add('active');
 
-leftSlideIndex = nextLeftSlideIndex
+  pageDots.forEach(function (dot) {
+    dot.classList.remove('active');
+  });
 
+  pageDots[nextLeftSlideIndex].classList.add('active');
+
+  leftSlideIndex = nextLeftSlideIndex;
 }
